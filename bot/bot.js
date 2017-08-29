8440c9c95cb73b4fa82960ed220c9a7467b789da
Solomon Tang <solomoncztang@gmail.com>
2017-08-28 17:30:39 -0700

    Change command detection logic to use a switch

--- bot/bot.js
+++ bot/bot.js
@@ -8,9 +8,18 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-  console.log(message);
-  if (message.content === 'ping') {
-    message.reply('pong');
+  // console.log(message);
+  if (message.content[0] !== config.prefix) return;
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  console.log('arguments, command', args, command);
+  console.log('message channel: ', message.channel.name, message.channel.id);
+  switch (command) {
+    case 'ping':
+      message.reply('pong');
+    default:
+      return;
   }
 });
 
