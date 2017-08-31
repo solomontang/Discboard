beb4ae2dd726cfef11d3831e28888f175bfc8b26
Solomon Tang <solomoncztang@gmail.com>
2017-08-30 19:19:11 -0700

    Refactored chatwheel to separate function

--- bot/bot.js
+++ bot/bot.js
@@ -1,10 +1,18 @@
 const Discord = require('discord.js');
 const config = require('../config.json')
+const axios = require('axios');
+const request = require('request');
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+
 const client = new Discord.Client();
+
+const ChatWheel = require('./modules/ChatWheel');
+
+var currentVoiceChannel;
 
 client.on('ready', () => {
   console.log('I am ready!');
-  // console.log(client);
 });
 
 client.on('message', message => {
@@ -13,14 +21,16 @@ client.on('message', message => {
 
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
-  console.log('arguments, command', args, command);
-  console.log('message channel: ', message.channel.name, message.channel.id);
-  switch (command) {
-    case 'ping':
-      message.reply('pong');
-    default:
-      return;
+  console.log(`Command: ${command} \t Arguments: ${args}`);
+  console.log('Message channel: ', message.channel.name, message.channel.id);
+  if (command === 'ping') {
+    message.reply('pong');
+    return;
+  } else if (command === 'c' || command === 'chat') {
+    currentVoiceChannel = ChatWheel(message, currentVoiceChannel, command, args);
+  } else {
+    return;
   }
 });
 
-console.log(client.login(config.token));
+client.login(config.token);
---
+++ bot/modules/ChatWheel.js
@@ -0,0 +1,41 @@
+module.exports = function ChatWheel(message, currentVoiceChannel, command, args) {
+  console.log('in chatwheel');
+  console.log(args);
+  if (!message.guild) return;
+  // Only try to join the sender's voice channel if they are in one themselves
+  if (message.member.voiceChannel) {
+    if (args[0] === 'join') {
+      currentVoiceChannel = message.member.voiceChannel.join();
+    } else if (args[0] === 'leave' && currentVoiceChannel ) {
+      currentVoiceChannel.then(connection => {
+        connection.disconnect();
+      });
+    } else if (currentVoiceChannel) {
+      currentVoiceChannel.then(connection => { // Connection is an instance of VoiceConnection
+        //LOCAL FILES
+        const dispatch = connection.playFile( `/Users/solomontang/Projects/Discboard/static/${args[0]}.wav`, {volume: 0.5} );
+
+        //TODO: STREAM REMOTE HOSTED FILE TO PLAYSTREAM
+
+        // axios({
+        //   method:'get',
+        //   url:'https://dota2.gamepedia.com/media/dota2.gamepedia.com/b/bf/Chatwheel_frog.wav',
+        //   responseType:'octet-stream'
+        // })
+        //   .then(function(response) {
+        //     // console.log(response.data);
+        //     connection.playStream(response.data);
+        //   });
+
+        //YOUTUBE STREAM
+        // const stream = ytdl('https://www.youtube.com/watch?v=XAWgeLF9EVQ', { filter : 'audioonly' });
+        // const dispatcher = connection.playStream(stream);
+      })
+      .catch(console.log);
+    }
+  } else {
+    message.reply('You need to join a voice channel first!');
+  }
+  return currentVoiceChannel;
+}
+
