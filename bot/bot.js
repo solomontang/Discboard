ef49d77cf6537f88fc696faacee16eface092b75
Solomon Tang <solomoncztang@gmail.com>
2017-08-31 12:32:37 -0700

    User must be in the same voice channel to issue chat command

--- bot/bot.js
+++ bot/bot.js
@@ -9,24 +9,22 @@ const client = new Discord.Client();
 
 const ChatWheel = require('./modules/ChatWheel');
 
-var currentVoiceChannel;
+var currentVoiceConnection = null;
 
 client.on('ready', () => {
   console.log('I am ready!');
 });
 
 client.on('message', message => {
-  // console.log(message);
   if (message.content[0] !== config.prefix) return;
 
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
+
   if (command === 'ping') {
     message.reply('pong');
-    return;
   } else if (command === 'c' || command === 'chat') {
-    currentVoiceChannel = ChatWheel(message, currentVoiceChannel, command, args);
-    return;
+    currentVoiceConnection = ChatWheel(message, currentVoiceConnection, command, args);
   } else {
     return;
   }
